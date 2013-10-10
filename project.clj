a97261840d3f3893c180400f4e3321da477bd6eb
Lucas Stadler <mi.au@papill0n.org>
2013-10-10 14:23:29 +0200

    Add timestamps to HN source.

--- project.clj
+++ project.clj
@@ -6,6 +6,7 @@
                  [ring "1.2.0"]
 
                  ; scraping
-                 [enlive "1.1.4"]]
+                 [enlive "1.1.4"]
+                 [com.joestelmach/natty "0.8"]]
   :plugins [[lein-ring "0.8.7"]]
   :ring {:handler arashi.server/app})
--- src/arashi/sources.clj
+++ src/arashi/sources.clj
@@ -4,16 +4,22 @@
 (defn fetch-html [url]
   (html/html-resource (java.net.URL. url)))
 
+(defn parse-date [date-string]
+  (-> (.parse (com.joestelmach.natty.Parser.) date-string) first .getDates first))
+
 (defn hackernews []
   (let [frontpage-html (fetch-html "https://news.ycombinator.com")
         titles (html/select frontpage-html [:td.title :a])
-        users-and-comments (html/select frontpage-html [:td.subtext :a])]
-    (map (fn [title [user comments]]
+        more-infos (html/select frontpage-html [:td.subtext])
+        times-ago (html/select more-infos [(html/text-pred #(.contains % "ago"))])
+        users-and-comments (html/select more-infos [:a])]
+    (map (fn [title [user comments] time-ago]
            {:url (get-in title [:attrs :href])
             :title (html/text title)
+            :timestamp (parse-date time-ago)
             :author (str "https://news.ycombinator.com/" (get-in user [:attrs :href]))
             :comments (str "https://news.ycombinator.com/" (get-in comments [:attrs :href]))})
-         titles (partition 2 users-and-comments))))
+         titles (partition 2 users-and-comments) times-ago)))
 
 (defn hackernews-post? [post]
   (.contains (:comments post) "news.ycombinator.com/"))
