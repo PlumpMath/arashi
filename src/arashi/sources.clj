842e22f68e31c61ca7de821b73c1d5b096385db5
Lucas Stadler <mi.au@papill0n.org>
2013-10-10 12:56:06 +0200

    Add news.ycombinator.com source.

--- src/arashi/sources.clj
+++ src/arashi/sources.clj
@@ -1,11 +1,19 @@
 (ns arashi.sources
   (:require [net.cgrand.enlive-html :as html]))
 
-(defn hackernews []
-  [] #_(json/parse (http/get "http://api.ihackernews.com/page?format=json")))
-
 (defn fetch-html [url]
   (html/html-resource (java.net.URL. url)))
+
+(defn hackernews []
+  (let [frontpage-html (fetch-html "https://news.ycombinator.com")
+        titles (html/select frontpage-html [:td.title :a])
+        users-and-comments (html/select frontpage-html [:td.subtext :a])]
+    (map (fn [title [user comments]]
+           {:url (get-in title [:attrs :href])
+            :title (html/text title)
+            :author (str "https://news.ycombinator.com/" (get-in user [:attrs :href]))
+            :comments (str "https://news.ycombinator.com/" (get-in comments [:attrs :href]))})
+         titles (partition 2 users-and-comments))))
 
 (defn twitter [username]
   (let [user-html (fetch-html (str "https://mobile.twitter.com/" username))
