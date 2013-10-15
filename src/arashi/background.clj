dfe1420b3fc3c2dc4c466972534523f47f3d2a63
Lucas Stadler <mi.au@papill0n.org>
2013-10-15 18:43:22 +0200

    Try fetching posts in the background.

---
+++ src/arashi/background.clj
@@ -0,0 +1,50 @@
+(ns arashi.background
+  "Fetches posts from various sources in a background thread.
+
+Uses a backoff to check non-frequently updated sources less often."
+  (:require [arashi.posts :as p]
+            [arashi.sources :as s]))
+
+(def min-interval (* 1 60))
+(def max-interval (* 1 24 60 60))
+
+(defn backoff-inc [n]
+  (min (* n 2) max-interval))
+
+(defn backoff-dec [n]
+  (max (/ n 2) min-interval))
+
+(defn update-posts [posts-ref new-posts]
+  (dosync
+   (let [posts @posts-ref
+         updated-posts (p/join posts new-posts)]
+     (ref-set posts-ref updated-posts)
+
+     (if (= (count posts) (count updated-posts))
+       backoff-inc
+       backoff-dec))))
+
+(defn backoff-fn [a f]
+  (fn backoff-fn* [interval]
+    (let [change-fn (f)]
+      (send-off a backoff-fn*)
+      (println interval)
+      (Thread/sleep (* 1000 interval))
+      (change-fn interval))))
+
+
+(defn fetch-posts [fetch-fns]
+  (let [posts (ref (p/posts-set))]
+    (doseq [fetch-fn fetch-fns]
+      (future
+        (let [a (agent min-interval)]
+          (send-off a (backoff-fn a #(update-posts posts (fetch-fn)))))))
+    posts))
+
+(defn example []
+  (def ps (fetch-posts [#(s/twitter "fogus") #(s/twitter "djspiewak") s/hackernews]))
+
+  (map :title @ps))
+
+;(example)
+;(doall (map println (map :title @ps)))
