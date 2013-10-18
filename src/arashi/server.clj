93f30629419abf96304fc69c5ea116fc1da043ad
Lucas Stadler <mi.au@papill0n.org>
2013-10-18 13:09:18 +0200

    Support serving data resources pretty printed.
    Now we can inspect the agent states neatly as they can be pretty printed
    and contain the agent state + the last error.

--- src/arashi/server.clj
+++ src/arashi/server.clj
@@ -1,5 +1,6 @@
 (ns arashi.server
-  (:use ring.adapter.jetty)
+  (:use ring.adapter.jetty
+        [ring.middleware.params :only (wrap-params)])
   (:use compojure.core)
   (:require [ring.util.response :as resp])
 
@@ -45,18 +46,40 @@
     err
     a))
 
-(defroutes app
+(defn pp-str [obj]
+  (let [w (java.io.StringWriter.)]
+    (clojure.pprint/pprint obj w)
+    (.toString w)))
+
+(defroutes app-routes
   (GET "/" []
        (apply str (r/posts-tmpl (reverse @posts))))
   (GET "/status" []
-       (-> (resp/response (prn-str (map pretty-agent @bg-fetching)))
+       (-> (resp/response
+            (reverse (sort-by (comp :last-error-t deref) @bg-fetching)))
            (resp/content-type "text/plain")))
   (GET "/posts.edn" []
-       (-> (resp/response (prn-str (reverse @posts)))
+       (-> (resp/response (reverse @posts))
            (resp/content-type "text/plain")))
   (POST "/superfeedr" req
         (prn (-> req :body slurp))
         "ok"))
+
+(defn pretty-print-edn [handler]
+  (fn [req]
+    (let [{{pretty "pretty"} :query-params} req
+          resp (handler req)
+          not-string? (not (instance? String (:body resp)))]
+      (if not-string?
+        (if (= pretty "true")
+          (update-in resp [:body] pp-str)
+          (update-in resp [:body] prn-str))
+        resp))))
+
+(def app
+  (-> app-routes
+      pretty-print-edn
+      wrap-params))
 
 (defn run! []
   (run-jetty app {:port 1234}))
