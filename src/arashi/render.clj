7144826f549e4c453b1d06b9d515df2ec4cffcf6
Lucas Stadler <mi.au@papill0n.org>
2013-10-20 15:59:23 +0200

    Render `:via`.

--- src/arashi/render.clj
+++ src/arashi/render.clj
@@ -9,12 +9,15 @@
     (str (.getProtocol url) "://" (.getHost url))))
 
 (defsnippet post-tmpl "arashi/posts.html" [:#posts [:.post (html/nth-of-type 1)]]
-  [{:keys [title url timestamp]}]
+  [{:keys [title url via timestamp]}]
   [:a] (html/do->
         (html/content title)
         (html/set-attr :href url))
   [:.timestamp] (html/content  (str "(" (.format (PrettyTime.) (or timestamp (Date.))) ")"))
-  [:.icon] (html/set-attr :src (str (root-url url) "/favicon.ico")))
+  [:.icon] (html/set-attr :src (str (root-url url) "/favicon.ico"))
+  [:.via :a] (html/do->
+              (html/content (-> (or via url) (java.net.URL.) .getHost))
+              (html/set-attr :href (or via url))))
 
 (deftemplate posts-tmpl "arashi/posts.html"
   [posts]
