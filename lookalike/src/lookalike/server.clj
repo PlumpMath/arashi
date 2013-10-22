940a3a3a79c045b6d4969a3ce65fbfe21a157fae
Lucas Stadler <mi.au@papill0n.org>
2013-10-22 15:06:59 +0200

    Support inspecting the cache.

--- lookalike/src/lookalike/server.clj
+++ lookalike/src/lookalike/server.clj
@@ -2,15 +2,28 @@
   (:use compojure.core
         [ring.middleware.params :only (wrap-params)]
         [ring.util.response :only (redirect response content-type)])
+  (:require [clojure.pprint :as pp]
+            [clojure.core.memoize :as memo])
 
   (:use [lookalike.cache :only (favicon-of)]))
+
+(defn pp-str [obj]
+  (let [w (java.io.StringWriter.)]
+    (pp/pprint obj w)
+    (.toString w)))
 
 (defroutes app-routes
   (GET "/favicon" {{url "url"} :params}
        (redirect (favicon-of url)))
   (GET "/favicon_url" {{url "url"} :params}
        (-> (response (favicon-of url))
-           (content-type "text/plain"))))
+           (content-type "text/plain")))
+  (GET "/cache" []
+       (let [cache (memo/snapshot favicon-of)]
+         (-> (response
+              (pp-str {:n (count cache)
+                       :cache cache}))
+             (content-type "text/plain")))))
 
 (def app
   (-> app-routes
