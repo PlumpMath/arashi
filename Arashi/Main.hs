e4f5125f433b396426fd2b99b876f923a3cc1430
Lucas Stadler <lu@papill0n.org>
2014-05-03 13:59:10 +0200

    use other modules in main.
    this makes development easier (just `cabal repl`) and prepares this
    stuff for more features.

--- Arashi/Main.hs
+++ Arashi/Main.hs
@@ -1,11 +1,22 @@
 import System.Environment (getArgs)
 import Control.Monad (forM_)
 
+import Data.EDN (decode)
+import Data.String.Conversions (convertString)
+
 import Arashi.Core (fromFetchedFeed)
+import Arashi.Config
+import Arashi.HTML
 
 main :: IO ()
 main = do
-    [url] <- getArgs
-    Just entries <- fromFetchedFeed url
-    forM_ entries $ \entry ->
-        print entry
+    args <- getArgs
+    case args of
+        [url] -> do
+            feed <- feedFromURI url
+            Just entries <- fromFetchedFeed feed
+            forM_ entries $ \entry ->
+                print entry
+        _ -> do
+            Just (Config uris) <- readFile "config.edn" >>= return . decode . convertString
+            forM_ uris putStrLn
